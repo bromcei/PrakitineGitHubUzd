119f6c9f6d7c4baa5223be225a0c4584bbe1c0c6
tomas.ceida <tomceid@gmail.com>
2022-09-13 19:41:19 +0300

    Add project files.

---
+++ PrakitineGitHubUzd/Program.cs
@@ -0,0 +1,15 @@
+﻿double kintamasis1 = 0;
+double kintamasis2 = 0;
+double kintamasis3 = 0;
+
+// 1 Variantas
+
+// 2 Variantas
+
+
+
+// 3 Variantas
+
+// Rezultatas
+
+Console.WriteLine($"Rezultatas: {kintamasis1 + kintamasis2 + kintamasis3}");
