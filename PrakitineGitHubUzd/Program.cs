10007020e1cf4c25bde3240655eb2d0ee8ad73aa
tomas.ceida <tomceid@gmail.com>
2022-09-13 19:59:18 +0300

    Pre Final commit

--- PrakitineGitHubUzd/Program.cs
+++ PrakitineGitHubUzd/Program.cs
@@ -3,7 +3,8 @@ double kintamasis2 = 0;
 double kintamasis3 = 0;
 
 // 1 Variantas
-
+double r1 = 6371;
+kintamasis1 = 4 * Math.PI * Math.Pow(r1, 2);
 // 2 Variantas
 
 double r2 = 6371;
@@ -15,5 +16,10 @@ Console.WriteLine($"Rutulio tūris: V = {kintamasis2}");
 // 3 Variantas
 
 // Rezultatas
-
-Console.WriteLine($"Rezultatas: {kintamasis1 + kintamasis2 + kintamasis3}");
+Console.WriteLine("*********************************");
+Console.WriteLine("* Viso kintamųjų: 3");
+Console.WriteLine($"* Kintamasis1; {kintamasis1}");
+Console.WriteLine($"* Kintamasis2; {kintamasis2}");
+Console.WriteLine($"* Kintamasis3; {kintamasis3}");
+Console.WriteLine($"* Suma: {kintamasis1 + kintamasis2 + kintamasis3}");
+Console.WriteLine($"* Visurkis: {(kintamasis1 + kintamasis2 + kintamasis3) / 3}");
